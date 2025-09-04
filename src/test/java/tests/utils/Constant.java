fc36b6a22370837e46c28c9a8e346fc53b663454
nazneen.rahman <itsnazn33n@gmail.com>
2025-09-04 17:50:06 +0600

    Complete the checkout flow: add items, proceed to checkout, fill required details, and complete the purchase

--- src/test/java/tests/utils/Constant.java
+++ src/test/java/tests/utils/Constant.java
@@ -4,6 +4,10 @@ public class Constant {
 
     public final static String login_username = "qa_testers@qabrains.com";
     public final static String login_password = "Password123";
+    public final static String E_login_username = "test@qabrains.com";
+    public final static String E_login_password = "Password123";
+    public final static String E_first_name= "QA";
+    public final static String E_last_name = "Rahman";
     public final static String wrong_login_username = "qa@qabrains.com";
     public final static String wrong_login_password = "Password1234";
     public final static String name = "Test User";
